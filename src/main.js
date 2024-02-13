f93e1be1238469bf63d8fa7b9a3d873296807f70
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-02-13 18:40:29 +0100

    'update'

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import dangIcon from './img/dang.svg';
-import errorIcon from './img/err.svg';
-import xIcon from './img/x.svg';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+import { renderImage } from './js/markup';
+import { fetchImage } from './js/api';
+import { onRejected } from './js/error-handler';
 
 const form = document.querySelector("#searchForm");
-const imageList = document.querySelector(".gallery");
 const loaderContainer = document.querySelector(".loader");
 
 form.addEventListener("submit", onSearch);
@@ -25,134 +18,10 @@ function onSearch(event) {
   fetchImage(keyWord)
     .then((images) => {
       renderImage(images);
-      loaderContainer.style.display = 'none';
     })
     .catch((error) => {
       onRejected(error);
-      loaderContainer.style.display = 'none';
-    });
+    }).finally(() => loaderContainer.style.display = 'none');
 
   form.reset();
-}
-
-function fetchImage(keyWord) {
-  const BASE_URL = 'https://pixabay.com/';
-  const END_POINT = 'api/';
-  const PARAMS = new URLSearchParams({
-    key: '42208232-118910d8102453b47e924ae6c',
-    q: keyWord,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true
-  });
-  const URL = `${BASE_URL}${END_POINT}?${PARAMS}`;
-
-  return fetch(URL)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`${response.status} - ${response.statusText}`);
-    });
-}
-
-function renderImage({ totalHits, hits }) {
-  if (parseInt(totalHits) > 0) {
-    const markup = hits.map(createElementGallery).join('');
-    imageList.innerHTML = markup;
-    lightbox.refresh();
-  } else {
-    imageList.innerHTML = '';
-    onWarning();
-  }
-}
-
-function onWarning() {
-  iziToast.warning({
-    title: 'Sorry,',
-    titleColor: '#FFFFFF',
-    message: 'there are no images matching your search query. Please try again!',
-    messageColor: '#FFFFFF',
-    messageSize: '16px',
-    backgroundColor: '#FFA000',
-    iconUrl: dangIcon,
-    position: 'center',
-    close: false,
-    buttons: [
-      [
-        `<button type="button" style="
-          background-color: #FFA000; 
-          width: 20px; 
-          height: 20px; 
-          padding: 5px">
-            <img style="
-              width: 10px; 
-              height: 10px" 
-              src=${xIcon}>
-        </button>`,
-        function (instance, toast) {
-          instance.hide({ transitionOut: 'fadeOut' }, toast);
-        },
-      ],
-    ]
-  });
-}
-
-function onRejected(error) {
-  iziToast.show({
-    title: 'Error',
-    titleColor: '#FFFFFF',
-    message: `${error}`,
-    messageColor: '#FFFFFF',
-    messageSize: '16px',
-    backgroundColor: '#EF4040',
-    iconUrl: errorIcon,
-    position: 'topRight',
-    close: false,
-    buttons: [
-      [
-        `<button type="button" style="
-          background-color: #EF4040; 
-          width: 20px; 
-          height: 20px; 
-          padding: 5px">
-            <img style="
-              width: 10px; 
-              height: 10px" 
-                src=${xIcon}>
-        </button>`,
-        function (instance, toast) {
-          instance.hide({ transitionOut: 'fadeOut' }, toast);
-        },
-      ],
-    ]
-  });
-}
-
-function createElementGallery({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-  return `
-  <li class="gallery-item">
-    <a class="gallery-link" href="${largeImageURL}">
-      <img class="gallery-image" src="${webformatURL}" alt="${tags}">
-    </a>
-    <ul class="item-img">
-      <li class="elem-img">
-        <p class="elem-name">Likes</p>
-        <p>${likes}</p>
-      </li>
-      <li class="elem-img">
-        <p class="elem-name">Views</p>
-        <p>${views}</p>
-      </li>
-      <li class="elem-img">
-        <p class="elem-name">Comments</p>
-        <p>${comments}</p>
-      </li>
-      <li class="elem-img">
-        <p class="elem-name">Downloads</p>
-        <p>${downloads}</p>
-      </li>
-    </ul>
-  </li>
-  `;
 }
